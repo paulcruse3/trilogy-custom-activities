8c7d056e7347afbb2d27e4917ebc203918d705d9
Paul Cruse III <paulcruse3@gmail.com>
2019-04-03 01:17:22 -0500

    final product

--- worker.js
+++ worker.js
@@ -1,59 +1,75 @@
 $(document).ready(function() {
-    var digger = {};
-    var bricker = {};
-    var contractor = {};
-    function getWorkers() {
-        digger = {
-            buckets: 0,
-            digDirt: function(){
-                this.buckets += Math.floor(Math.random() * 100);
-            },
-            getBuckets : function() {
-                buckets = this.buckets;
-                this.buckets = 0;
-                return buckets;
-            }
-        };
-        bricker = {
-            bricks: 0,
-            buildBricks: function(buckets){
-                this.bricks = this.bricks +  Math.floor(buckets / 5);
-            },
-            giveBricks: function(){
-                bricks = this.bricks;
-                this.bricks = 0;
-                return bricks;
-
-            }
-        };
-        contractor = {
-            houseParts: ['H', 'O', 'U', 'S', 'E'],
-            currentPart: 0,
-            buildHouse: function(bricks){
-                console.log(bricks % 100);
-                if (Math.floor(bricks / 100) > 0 && bricks != 0){
-                    $("#plot").append(this.houseParts[this.currentPart]);
-                    this.currentPart++;
+    var digger = {
+        buckets: 0,
+        digDirt: function(){
+            this.buckets = this.buckets + Math.floor(Math.random() * 100);
+        },
+        getBuckets: function(){
+            localBuckets = this.buckets;
+            this.buckets = 0;
+            return localBuckets;
+        }
+    };
+    var bricker = {
+        bricks: 0,
+        buildBricks: function(buckets){
+            this.bricks = this.bricks + Math.floor(buckets / 5);
+        },
+        getBricks: function(){
+            localBricks = this.bricks;
+            this.bricks = 0;
+            return localBricks;
+        }
+    };
+    var contractor = {
+        bluePrint: ['H', 'O', 'U', 'S', 'E'],
+        currentHousePart: 0,
+        bricks: 0,
+        buildHouse: function(bricks){
+            this.bricks = this.bricks + bricks;
+            houseParts = Math.floor(this.bricks / 100);
+            if (houseParts >= 1){
+                for (var i=0; i < houseParts; i++){
+                    $("#plot").append(this.bluePrint[this.currentHousePart]);
+                    this.currentHousePart++;
                 }
+                this.bricks = 0;
             }
-        };
+        }
+    };
+    function determineVisibleButtons(){
+        if (digger.buckets >= 5){
+            $("#buildBricks").removeClass("hide");
+        } else {
+            $("#buildBricks").addClass("hide");
+        }
+        if (bricker.bricks >= 100){
+            $("#buildHouse").removeClass("hide");
+        } else {
+            $("#buildHouse").addClass("hide");
+        }
+    }
+    function printCount(){
+        $("#bucketCounter").text("buckets: " + digger.buckets);
+        $("#brickCounter").text("bricks: " + bricker.bricks);
     }
     $("#getWorkers").on("click", function(){
-        getWorkers();
-        $("#getWorkers").addClass("hide");
         $("#digDirt").removeClass("hide");
-        $("#buildBricks").removeClass("hide");
-        $("#buildHouse").removeClass("hide");
+        $("#getWorkers").addClass("active");
     });
     $("#digDirt").on("click", function(){
         digger.digDirt();
-        console.log('buckets got:'+ digger.buckets);
+        printCount();
+        determineVisibleButtons();
     });
     $("#buildBricks").on("click", function(){
         bricker.buildBricks(digger.getBuckets());
-        console.log('bricks built:'+ bricker.bricks);
+        printCount();
+        determineVisibleButtons();
     });
     $("#buildHouse").on("click", function(){
-        contractor.buildHouse(bricker.giveBricks());
+        contractor.buildHouse(bricker.getBricks());
+        printCount();
+        determineVisibleButtons();
     });
 });
