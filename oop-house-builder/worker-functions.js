7fc3d8f55d87d2b9d64ccd3398208b681134702f
Paul Cruse III <paulcruse3@gmail.com>
2020-04-21 22:04:39 -0500

    stuff

---
+++ oop-house-builder/worker-functions.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+    var bucketsOfDirt = 0;
+    var bricks = 0;
+    var word = '';
+                // H 1st
+                // HO 2nd
+                // HOU 3rd
+    var house = ['H','O','U','S','E'];
+                // ['O','U','S','E'];
+                // ['U','S','E'];
+                // ['S','E'];
+    var getDirt = function(buckets){
+        buckets = buckets + Math.floor(Math.random() * 100);
+        console.log('buckets', buckets);
+        return buckets;
+    }
+    var getBricks = function(bricks, buckets){
+        var bucketsToBricks = Math.floor(buckets / 10);
+        bricks = bricks + bucketsToBricks;
+        buckets = buckets - (bucketsToBricks * 10);
+        console.log('buckets', buckets);
+        console.log('bricks', bricks);
+        return [bricks, buckets];
+    }
+    var getHouse = function(house, bricks, word){
+        var partOfHouse = Math.floor(bricks / 100);
+        for (var i = 0; i < partOfHouse; i++){
+            var letter = house.shift();
+            word = word + letter;
+            console.log(letter, word)
+        }
+        bricks = bricks - (Math.floor(bricks / 100) * 100);
+        return [bricks, house, word];
+    }
+
+// ===================== UI CODE Below =============================
+    $('#getWorkersBtn').on('click', function(){
+        $('#digDirtBtn').show();
+        $('#buildBricksBtn').show();
+        $('#buildHouseBtn').show();
+    });
+    $('#digDirtBtn').on('click', function(){
+        bucketsOfDirt = getDirt(bucketsOfDirt);
+    });
+    $('#buildBricksBtn').on('click', function(){
+        var brickResults = getBricks(bricks, bucketsOfDirt);
+        bucketsOfDirt = brickResults[1];
+        bricks = brickResults[0];
+    });
+    $('#buildHouseBtn').on('click', function(){
+        var houseResults = getHouse(house, bricks, word);
+        bricks = houseResults[0];
+        house = houseResults[1];
+        word = houseResults[2];
+    });
+});
